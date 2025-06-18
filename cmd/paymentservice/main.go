f362c7c5105f666355538adb7eadf6ad3e14f80a
rk5634 <rajiv0099c@gmail.com>
2025-06-18 09:08:18 +0530

    made some changes in main.go

--- cmd/paymentservice/main.go
+++ cmd/paymentservice/main.go
@@ -26,7 +26,7 @@ func main() {
 	// Routes
 	router.GET("/health", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{"status": "ok",
-			"message": "Hi main - Payment service is running"})
+			"message": "Hi prod - Payment service is running"})
 	})
 
 	// Start server
